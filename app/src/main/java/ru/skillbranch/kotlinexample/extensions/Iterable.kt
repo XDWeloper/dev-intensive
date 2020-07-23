5059b13467457ad7ca84ff13ccbd47f42313db4a
XDWeloper <Dmitryxdw@mail.ru>
2020-07-23 14:01:01 +0300

    commit

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -1,2 +1,15 @@
 package ru.skillbranch.kotlinexample.extensions
 
+fun <T>List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T>{
+val mutList = this.toMutableList()
+
+   for (i in (mutList.size -1)  downTo 0){
+       mutList.removeAt(i)
+       if(predicate(this[i])) break
+    }
+
+    if(mutList.size == 0) return this
+    else return mutList.toList()
+}
+
+
