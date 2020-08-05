ae56bceb76f1bbf054a1da75568d176e5270ca18
XDWeloper <Dmitryxdw@mail.ru>
2020-08-05 10:51:18 +0300

    commit

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -2,29 +2,25 @@ package ru.skillbranch.skillarticles.ui.custom.behaviors
 
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.math.MathUtils
 import androidx.core.view.ViewCompat
 import ru.skillbranch.skillarticles.ui.RootActivity
 import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
-import ru.skillbranch.skillarticles.ui.custom.Bottombar
 import ru.skillbranch.skillarticles.viewmodels.ArticleViewModel
 
-class BottombarBehavior : CoordinatorLayout.Behavior<Bottombar>() {
+class SubmenuBehavior : CoordinatorLayout.Behavior<ArticleSubmenu>() {
 
-    private var articleSubMenu : ArticleSubmenu? = null
     private var viewModel : ArticleViewModel? = null
 
 
     override fun onStartNestedScroll(
         coordinatorLayout: CoordinatorLayout,
-        child: Bottombar,
+        child: ArticleSubmenu,
         directTargetChild: View,
         target: View,
         axes: Int,
         type: Int
     ): Boolean {
-        if(articleSubMenu == null)
-            articleSubMenu = if (coordinatorLayout.childCount > 2) coordinatorLayout.getChildAt(2) as ArticleSubmenu else null
+
         if(viewModel == null) {
             val rootActivity = coordinatorLayout.context as RootActivity
             viewModel = rootActivity.getViewModel()
@@ -35,7 +31,7 @@ class BottombarBehavior : CoordinatorLayout.Behavior<Bottombar>() {
 
     override fun onNestedPreScroll(
         coordinatorLayout: CoordinatorLayout,
-        child: Bottombar,
+        child: ArticleSubmenu,
         target: View,
         dx: Int,
         dy: Int,
@@ -43,14 +39,12 @@ class BottombarBehavior : CoordinatorLayout.Behavior<Bottombar>() {
         type: Int
     ) {
 
-//        if(articleSubMenu!= null && dy > 0 && viewModel!!.currentState.isShowMenu){
-//            articleSubMenu!!.close()
-//        }
-        if(articleSubMenu!= null && dy < 0 && viewModel!!.currentState.isShowMenu){
-            articleSubMenu!!.open()
+        if(dy > 0 && viewModel!!.currentState.isShowMenu){
+            child.close()
         }
-
-        child.translationY = MathUtils.clamp(child.translationY.plus(dy) , 0f, child.minHeight.toFloat())
+        if(dy < 0 && viewModel!!.currentState.isShowMenu){
+            child.open()
+        }
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
     }
 }
